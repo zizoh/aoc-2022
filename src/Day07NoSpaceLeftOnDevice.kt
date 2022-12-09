a08f1ca09d93c34b0f86004f4b1061e988be6c39
zizoh <zizoh.anto@getir.com>
2022-12-09 02:29:15 +0100

    Refactor Day07

--- src/Day07NoSpaceLeftOnDevice.kt
+++ src/Day07NoSpaceLeftOnDevice.kt
@@ -9,23 +9,23 @@ fun getSumOfTotalSizes(input: List<String>): Int {
     val directories = mutableListOf(root)
     for (index in 1..input.lastIndex) {
         val commandOrOutput = input[index]
+        val currentDirectory = directories.last()
         if (commandOrOutput.isMoveInCommand()) {
             val directoryName = commandOrOutput.getDirectoryName()
-            val tempCurrentDirectory = directories.last().children[directoryName]!!
+            val tempCurrentDirectory = currentDirectory.children[directoryName]!!
             directories.add(tempCurrentDirectory)
         } else if (commandOrOutput.isMoveOutCommand()) {
             directories.removeLast()
-        }
-        else if (commandOrOutput.isDirectoryOutput()) {
+        } else if (commandOrOutput.isDirectoryOutput()) {
             val directoryName = commandOrOutput.getDirectoryName()
             val directory = Node(directoryName, value = "")
             nodes.add(directory)
-            directories.last().children[directoryName] = directory
+            currentDirectory.children[directoryName] = directory
         } else if (commandOrOutput.isFileOutput()) {
             val fileName = commandOrOutput.getFileName()
             val file = Node(fileName, commandOrOutput.getFileSize())
             nodes.add(file)
-            directories.last().children[fileName] = file
+            currentDirectory.children[fileName] = file
         }
     }
     return 0
